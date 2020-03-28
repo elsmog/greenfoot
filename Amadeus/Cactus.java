351403a8fa02d50c3b92776ac596682bf36f5846
Elyssa Kober <elsmog@gmail.com>
2020-03-28 13:33:27 -0400

    Amadeus: Working scenario up to exercise 8.18.

--- Amadeus/Cactus.java
+++ Amadeus/Cactus.java
@@ -8,12 +8,50 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Cactus extends Actor
 {
+    
+    
     /**
      * Act - do whatever the Cactus wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        moveAround();
     }    
+    
+    public void moveAround()
+    {
+        MouseInfo mouseInfo = Greenfoot.getMouseInfo();
+        if (mouseInfo != null)
+        {
+            // move randomly
+            if (Greenfoot.mouseClicked(this))
+            {
+                setLocation(Greenfoot.getRandomNumber(400), Greenfoot.getRandomNumber(300));
+                Greenfoot.playSound("eating.wav");
+            
+            }
+            
+            else if (!Greenfoot.mouseClicked(this))
+            {
+                int buttonClicked = mouseInfo.getButton();
+            
+                //move to the right
+                if (buttonClicked == 1) 
+                {
+                    setRotation(0);
+                    move(10);
+                    Greenfoot.playSound("beebo.wav");
+                }
+            
+                //move to the left
+                if (buttonClicked == 3)
+                {
+                    setRotation(0);
+                    move(-10);
+                    Greenfoot.playSound("maclunkey.wav");
+                }
+            }
+        }
+    }
 }
